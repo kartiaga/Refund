bae62cd49ae07448934e6d9effc751296c1ccacd
kartiaga <kaiqueartiagaluz@gmail.com>
2025-04-16 15:38:13 -0300

    currency input format

---
+++ scripts.js
@@ -0,0 +1,25 @@
+// Seleciona os elementos do fórmulário
+const amount = document.getElementById("amount");
+
+// Captura o evento de input para formatar o valor.
+amount.oninput = () => {
+  // Obtém o valor atual do input e remove os numéricos.
+  let value = amount.value.replace(/\D/g, "")
+
+  // Transforma o valor em centavos (exemplo: 150/100 = 1.5 que é equivalente a R$ 1.50).
+  value = Number(value) / 100
+
+  // Atualiza o valor do input.
+  amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value) {
+  // Formata o valor no padrão BRL (Real brasileiro).
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  })
+
+  // Retorna o valor formatado.
+  return value
+}
