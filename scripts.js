450b9b6dd51c039bd7700e81b6515290889dec4c
kartiaga <kaiqueartiagaluz@gmail.com>
2025-04-29 22:48:38 -0300

    limpando os inputs após adicionar item

--- scripts.js
+++ scripts.js
@@ -96,6 +96,9 @@ function expenseAdd(newExpense) {
     // Adiciona o item na lista
     expenseList.append(expenseItem)
 
+    // Limpa o fórmulário
+    formClear()
+
     // Atualiza os totais
     updateTotals()
 
@@ -169,3 +172,13 @@ expenseList.addEventListener("click", function (event){
   // Atualiza os totais
   updateTotals()
 })
+
+function formClear(){
+  // Limpa os inputs
+  expense.value = ""
+  category.value = ""
+  amount.value = ""
+
+  // Coloca o foco no input de nome
+  expense.focus()
+}
